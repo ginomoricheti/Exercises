c349d0245f84d8050967b49bd539f8aedc43ac0f
ginomoricheti <115089986+ginomoricheti@users.noreply.github.com>
2024-04-26 13:53:09 -0300

    ex41.1

--- HTML-CSS-JS/41-qr_generator/scripts.js
+++ HTML-CSS-JS/41-qr_generator/scripts.js
@@ -1,5 +1,13 @@
 // Get DOM elements.
+const inputText = document.getElementById('link');
+const imgQR = document.getElementById('qr-image');
 
+// API url.
+const apiUrl = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=';
 
-// Game variables.
-
+// Generate QR.
+function getQR(){
+  imgQR.src = `${apiUrl}${inputText.value}`;
+  imgQR.style.display = 'block';
+  inputText.value = '';
+}
