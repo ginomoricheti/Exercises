9803cf0a8873d8d4c8580854e8e18006e57804fd
ginomoricheti <115089986+ginomoricheti@users.noreply.github.com>
2024-04-28 19:49:19 -0300

    ex46

---
+++ HTML-CSS-JS/46-popup_modal/scripts.js
@@ -0,0 +1,14 @@
+// Get DOM elements.
+let popUp = document.getElementById('popup');
+
+// Others variables.
+
+// Function to Open Popup.
+function openPopup(){
+  popUp.classList.add('open-popup');
+}
+
+// Function to close Popup.
+function closePopup(){
+  popUp.classList.remove('open-popup');
+}
