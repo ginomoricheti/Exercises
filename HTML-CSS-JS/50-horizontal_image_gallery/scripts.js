a16c31fa0264c0f90ff23b5d7392c0d7afd08a1b
ginomoricheti <115089986+ginomoricheti@users.noreply.github.com>
2024-04-30 12:43:45 -0300

    ex50

--- HTML-CSS-JS/50-horizontal_image_gallery/scripts.js
+++ HTML-CSS-JS/50-horizontal_image_gallery/scripts.js
@@ -1,5 +1,22 @@
 // Get DOM elements.
+let scrollContainer = document.querySelector('.gallery');
+let backBtn = document.getElementById('prev-btn');
+let nextBtn = document.getElementById('next-btn');
 
+// Scroll function.
+scrollContainer.addEventListener('wheel', (e)=> {
+  e.preventDefault();
+  scrollContainer.scrollLeft += e.deltaY;
+  scrollContainer.style.scrollBehavior = 'auto';
+})
 
-// Game variables.
+// Next/Prev Button Function.
+backBtn.addEventListener('click', ()=> {
+  scrollContainer.style.scrollBehavior = 'smooth';
+  scrollContainer.scrollLeft -= 900;
+})
 
+nextBtn.addEventListener('click', ()=> {
+  scrollContainer.style.scrollBehavior = 'smooth';
+  scrollContainer.scrollLeft += 900;
+})
