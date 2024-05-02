0fae91d04dd377913dad326c30eb79172a7ddb5c
ginomoricheti <115089986+ginomoricheti@users.noreply.github.com>
2024-05-02 12:44:48 -0300

    ex59.1

--- HTML-CSS-JS/59-product_page_design/scripts.js
+++ HTML-CSS-JS/59-product_page_design/scripts.js
@@ -3,24 +3,24 @@ let img = document.getElementById('product-img');
 let btn = document.getElementsByClassName('btn');
 
 // Changing photo function.
-btn[0].onclick = ()=>{
-  img.src = '/HTML-CSS-JS/59-product_page_design/images/image1.png'
-  for(bt of btn){
+btn[0].onclick = (event) => {
+  img.src = '/HTML-CSS-JS/59-product_page_design/images/image1.png';
+  for (bt of btn) {
     bt.classList.remove('active');
   }
-  this.classList.add('active');
-}
-btn[1].onclick = ()=>{
-  img.src = '/HTML-CSS-JS/59-product_page_design/images/image2.png'
-  for(bt of btn){
+  event.target.classList.add('active');
+};
+btn[1].onclick = (event) => {
+  img.src = '/HTML-CSS-JS/59-product_page_design/images/image2.png';
+  for (bt of btn) {
     bt.classList.remove('active');
   }
-  this.classList.add('active');
-}
-btn[2].onclick = ()=>{
-  img.src = '/HTML-CSS-JS/59-product_page_design/images/image3.png'
-  for(bt of btn){
+  event.target.classList.add('active');
+};
+btn[2].onclick = (event) => {
+  img.src = '/HTML-CSS-JS/59-product_page_design/images/image3.png';
+  for (bt of btn) {
     bt.classList.remove('active');
   }
-  this.classList.add('active');
-}
+  event.target.classList.add('active');
+};
