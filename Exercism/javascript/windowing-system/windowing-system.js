e527d0e5c9a0302648cbc096298177bada4dd74a
Gino Morichetti <ginomorichetti@gmail.com>
2025-07-07 18:44:38 -0300

    Ex26 - Prototypes & Classes

---
+++ Exercism/javascript/windowing-system/windowing-system.js
@@ -0,0 +1,73 @@
+// @ts-check
+
+/**
+ * Implement the classes etc. that are needed to solve the
+ * exercise in this file. Do not forget to export the entities
+ * you defined so they are available for the tests.
+ */
+// @ts-check
+
+class Size {
+  constructor(width = 80, height = 60) {
+    this.width = width;
+    this.height = height;
+  }
+
+  resize(newWidth, newHeight) {
+    this.width = newWidth;
+    this.height = newHeight;
+  }
+}
+
+class Position {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+
+  move(newX, newY) {
+    this.x = newX;
+    this.y = newY;
+  }
+}
+
+class ProgramWindow {
+  constructor() {
+    this.screenSize = new Size(800, 600);
+    this.size = new Size();
+    this.position = new Position();
+  }
+
+  resize(newSize) {
+    const maxWidth = this.screenSize.width - this.position.x;
+    const maxHeight = this.screenSize.height - this.position.y;
+
+    const newWidth = Math.max(1, Math.min(newSize.width, maxWidth));
+    const newHeight = Math.max(1, Math.min(newSize.height, maxHeight));
+
+    this.size.resize(newWidth, newHeight);
+  }
+
+  move(newPosition) {
+    const maxX = this.screenSize.width - this.size.width;
+    const maxY = this.screenSize.height - this.size.height;
+
+    const newX = Math.max(0, Math.min(newPosition.x, maxX));
+    const newY = Math.max(0, Math.min(newPosition.y, maxY));
+
+    this.position.move(newX, newY);
+  }
+}
+
+function changeWindow(window) {
+  const newSize = new Size(400, 300);
+  const newPosition = new Position(100, 150);
+
+  window.resize(newSize);
+  window.move(newPosition);
+
+  return window;
+}
+
+// Exportamos para pruebas
+export { Size, Position, ProgramWindow, changeWindow };
